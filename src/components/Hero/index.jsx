f71fe8f7997140f57ad5980e784d84d0b3f95d48
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2023-06-09 21:50:31 +0200

    make changes to imgs

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,7 +8,7 @@ function Hero() {
         <h2>Frontend-developer</h2>
         <p>I am finishing a two year higher professional degree in frontend-development in the spring of 2023. I have a masterdegree in political science, and I have experience working as a project manager and an IT-manager prior to my development-studies. Feel free to check out my portfolio, and contact me regarding anything.</p>
       </div>
-      <div className={styles.image}><img src="src/assets/profile.png" alt="profile picture of me" /></div>
+      <div className={styles.image}><img src="/assets/profile.png" alt="profile picture of me" /></div>
 
 
     </div>
